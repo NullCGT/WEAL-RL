3bd65e224dcdb75a7efd31366f6e34d2256be0e3
Kestrel Gregorich-Trevor <gregoric@grinnell.edu>
2021-12-22 11:04:41 -0600

    Refine the build script.
    Selecting an incorrect option will now cause the script to loop, as it should have originally.

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-printf "Choose a frontend:\n"
-printf "  1) ncurses (default)\n"
-printf "  2) pdcurses (recommended for Windows users)\n"
-read response
-
-if [ $response == 2 ]
-    then
-	export WEAL_PORT=pdcurses
-    else
-	export WEAL_PORT=ncurses
-fi
-printf "Building WEAL for $WEAL_PORT.\n"
+while [ 1 ]
+do
+    printf "Choose a frontend:\n"
+    printf "  1) ncurses (default)\n"
+    printf "  2) pdcurses (recommended for Windows users)\n"
+    read response
+    if [ $response == 2 ]
+        then
+        export GRAPHICAL_PORT=pdcurses
+        break
+    elif [ $response == 1 ]
+        then
+        export GRAPHICAL_PORT=ncurses
+        break
+    fi
+done
 printf "===============================\n"
+printf "Building WEAL for $GRAPHICAL_PORT.\n"
+printf "===============================\n\n"
 make all
+printf "\n===============================\n"
+printf "Finished building WEAL for $GRAPHICAL_PORT.\n"
+printf "===============================\n"
 exit 0
